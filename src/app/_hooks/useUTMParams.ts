af41fe3167d7d5c7b0f64c7ec7475535e30d535f
nabby27 <26057762+nabby27@users.noreply.github.com>
2025-06-29 11:49:15 +0200

    feat: add custom hook for UTM parameter management
    - Introduced `useUTMParams` hook to manage UTM parameters from URL and session storage.
    - Implemented functions to retrieve, save, and mix UTM parameters for enhanced tracking.
    - Added TypeScript interfaces for better type safety and clarity in UTM handling.

---
+++ src/app/_hooks/useUTMParams.ts
@@ -0,0 +1,85 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+const STORAGE_KEY = 'autofork.utm_params'
+
+export interface SourceUTMs {
+    utm_source: string
+    utm_medium: string
+    utm_campaign: string
+}
+
+export interface ContextUTMs {
+    utm_content: string
+    utm_term: string
+}
+
+export interface UTMParams extends SourceUTMs, ContextUTMs { }
+
+export function useUTMParams(contextUTMs: ContextUTMs): Partial<UTMParams> {
+    const [utmParams, setUtmParams] = useState<Partial<UTMParams>>(contextUTMs)
+
+    useEffect(() => {
+        const utms = getUTMs(contextUTMs)
+        setUtmParams(utms)
+    }, [contextUTMs])
+
+    return utmParams
+}
+
+function getUTMs(contextUTMs: ContextUTMs): Partial<UTMParams> {
+    const urlSourceUTMs = getSourceUTMsFromURL()
+    if (urlSourceUTMs) {
+        saveSourceUTMsToStorage(urlSourceUTMs)
+        return mixUTMs(urlSourceUTMs, contextUTMs)
+    }
+    
+    const storedSourceUTMs = getStoredSourceUTMs()
+    if (storedSourceUTMs) {
+        return mixUTMs(storedSourceUTMs, contextUTMs)
+    }
+    
+    return contextUTMs
+}
+
+function getSourceUTMsFromURL(): SourceUTMs | null {
+    const urlParams = new URLSearchParams(window.location.search)
+    
+    const sourceUTMs: SourceUTMs = {
+        utm_source: urlParams.get('utm_source') || '',
+        utm_medium: urlParams.get('utm_medium') || '',
+        utm_campaign: urlParams.get('utm_campaign') || '',
+    }
+    
+    const hasSourceUTM = Object.values(sourceUTMs).some(value => value !== '')
+    if (hasSourceUTM) {
+        return sourceUTMs
+    }
+
+    return null
+}
+
+function getStoredSourceUTMs(): SourceUTMs | null {
+    try {
+        const stored = sessionStorage.getItem(STORAGE_KEY)
+        return stored ? JSON.parse(stored) : null
+    } catch {
+        return null
+    }
+}
+
+function saveSourceUTMsToStorage(sourceUTMs: SourceUTMs): void {
+    try {
+        sessionStorage.setItem(STORAGE_KEY, JSON.stringify(sourceUTMs))
+    } catch {
+        console.error('Error saving source UTM params to session storage')
+    }
+}
+
+export function mixUTMs(sourceUTMs: SourceUTMs, contextUTMs: ContextUTMs): UTMParams {
+    return {
+        ...sourceUTMs,
+        ...contextUTMs
+    }
+}
